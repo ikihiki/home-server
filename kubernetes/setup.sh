a3516552ca03ee326670beff1f517089c4ec2676
伊吹英晃 <ibuki-90@hotmail.co.jp>
2023-07-19 00:21:49 +0900

    fix ansible

---
+++ kubernetes/setup.sh
@@ -0,0 +1,4 @@
+curl -o tekton/pipeline.yaml  https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml 
+curl -o tekton/triggers-release.yaml https://storage.googleapis.com/tekton-releases/triggers/latest/release.yaml
+curl -o tekton/triggers-interceptors.yaml https://storage.googleapis.com/tekton-releases/triggers/latest/interceptors.yaml 
+curl -o tekton/dashboard.yaml https://storage.googleapis.com/tekton-releases/dashboard/latest/release-full.yaml
